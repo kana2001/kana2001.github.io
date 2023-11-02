661b499ea3dbda47b185c355b609aaa9a9a4f37d
kana2001 <71416354+kana2001@users.noreply.github.com>
2023-11-01 23:20:54 -0400

    need to fix the font of the dark mode toggle

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import AboutMe from './components/AboutMe';
 import WorkExperience from './components/WorkExperience';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { TypeAnimation } from 'react-type-animation';
-import { ChakraProvider, Switch } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
 import DarkModeButton from './components/DarkModeButton';
+import Switch from '@mui/material/Switch';
+import { FormControlLabel } from '@mui/material';
 
 function App() {
   const [darkMode, setDarkMode] = useState(false); // starts in light mode
@@ -25,7 +27,16 @@ function App() {
             {/* <button onClick={toggleDarkMode}>
               {darkMode ? 'Light Mode' : 'Dark Mode'}
             </button> */}
-            <DarkModeButton></DarkModeButton>
+
+            {/* <DarkModeButton></DarkModeButton> */}
+
+            {/* TODO: Fix font of dark mode */}
+            <FormControlLabel
+              value="bottom"
+              control={<Switch onChange={toggleDarkMode} />}
+              label="Dark Mode"
+              labelPlacement="bottom"
+            />
 
           </nav>
           <h1>Hello, I'm Adi.</h1>
