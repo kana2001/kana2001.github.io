35fc350c98e6876f4b55908b59737fb9e1483e48
kana2001 <71416354+kana2001@users.noreply.github.com>
2023-11-02 20:57:18 -0400

    added favicon and title

--- src/components/DarkModeToggle.js
+++ src/components/DarkModeToggle.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styles from '../styles/DarkModeButton.module.css';
-import "react-toggle/style.css"
 import { DarkModeSwitch } from 'react-toggle-dark-mode';
 import { useSelector, useDispatch } from 'react-redux';
 import { toggleDarkMode, selectDarkMode } from '../features/themeSlice';
